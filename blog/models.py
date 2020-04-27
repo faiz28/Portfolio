037f78fe52420f896a9eb2f5633afad1a3e7a64e
faiz <faizcse28@gmail.com>
2020-04-28 04:13:36 +0600

    Complite Project

--- blog/models.py
+++ blog/models.py
@@ -7,16 +7,13 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     body = models.TextField()
-    image = models.ImageField(upload_to='image/')
+    image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title
 
+    def summary(self):  
+        return self.body[:100]
 
-#add the blog app to the setting
-
-#create a migration
-
-#migrate
-
-
-#Add to the admin
-
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
